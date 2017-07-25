84d4646bed574b6c8e1978fdb1a17e7b1c7e521e
Jonathan Roig <jroig@Jonathans-MacBook-Pro.local>
2017-07-24 22:55:36 -0700

    fix for ios chrome

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var emojiurlifier = function (emojiDomain, standardDomain) {
   console.log('Emoji Domain Progressive Enhancement by https://i❤️.ws')
   var wl = window.location;
   var useEmojiDomain = /^((?!chrome).)*safari/i.test(navigator.userAgent);
+  if (navigator.userAgent.indexOf('CriOS') > -1) {
+    useEmojiDomain = false;
+  }
   var currentUrl = wl.protocol + '//' + wl.hostname;
   var outputUrl = useEmojiDomain ? emojiDomain : standardDomain;
   if (currentUrl === outputUrl) {
